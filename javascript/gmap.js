4b025a4a0930c1a98356aff5755f4a8459360f5f
Wei Shi <weishi@andrew.cmu.edu>
2012-04-05 11:20:18 -0400

    Added Google Map API and location fetching

---
+++ javascript/gmap.js
@@ -0,0 +1,44 @@
+function success(position) {
+  var s = document.querySelector('#status');
+  
+  if (s.className == 'success') {
+    // not sure why we're hitting this twice in FF, I think it's to do with a cached result coming back    
+    return;
+  }
+  
+  
+  
+ 
+  var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  var myOptions = {
+    zoom: 15,
+    center: latlng,
+    mapTypeControl: false,
+    navigationControlOptions: {style: google.maps.NavigationControlStyle.SMALL},
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("mapcanvas"), myOptions);
+  
+  var marker = new google.maps.Marker({
+      position: latlng, 
+      map: map, 
+      title:"You are here! (within a "+position.coords.accuracy+" meter radius)"
+  });
+  var message="("+position.coords.latitude+","+position.coords.longitude+") within "+position.coords.accuracy+"m";
+  s.innerHTML = "found you at "+message;
+  s.className = 'success';
+}
+
+function error(msg) {
+  var s = document.querySelector('#status');
+  s.innerHTML = typeof msg == 'string' ? msg : "failed";
+  s.className = 'fail';
+  
+  // console.log(arguments);
+}
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(success, error);
+} else {
+  error('not supported');
+}
